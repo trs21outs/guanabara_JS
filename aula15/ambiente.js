f4ca4a55e83b10ef2f171c9dde9d4162785232aa
Thiago Ribeiro <80134828+trs21outs@users.noreply.github.com>
2021-03-31 01:59:06 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,28 @@
+
+/*let lista =[800,546,2,63,99,45]
+
+function comparaNumeros(a,b) { 
+    if (a == b) {
+        return 0;} 
+    if (a < b) {
+        return -1;}
+    if (a > b) {
+        return 1;}
+     }
+
+     console.log(lista)
+
+     lista.sort(comparaNumeros)
+
+     console.log(lista)*/
+
+
+     let numero = 123456789;
+     let numConvToString = String(numero)
+     let matriz = Array.from(numConvToString)
+     let invMatriz = matriz.reverse()
+     console.log(invMatriz)
+     
+     
+
+
